96cb7431787f57a0e9584bc38cca1337fae7f7df
Jerry Buaba <49881677+buabaj@users.noreply.github.com>
2022-10-18 23:47:36 +0000

    complete project

--- key_guard.py
+++ key_guard.py
@@ -1,34 +1,51 @@
-from cgi import print_form
-from genericpath import isdir
 import os
 from glob import glob
 import click
-
-guarded_words = ['key =', 'access_key =', 'auth_key =',
-                 'password =', 'secret =', 'token =', 'access_token =']
-
-exempted_files = ['key_guards.py']
-exempted_extensions = ['.jpg', '.png']
 
 
 cwd = os.getcwd()
 
 
-@click.command()
-@click.option('-a', '--add', help='Add a new word to the list of guarded words')
-@click.option('-r', '--remove', help='Remove a word from the list of guarded words')
-@click.option('-x', '--exclude', help='Exclude a file from the scan')
-@click.option('-i', '--include', help='Include a file in the scan')
+@click.command(name='Key Guard')
+@click.option('-e', '--exempt', nargs=1, help='exempt a file from scanning by removing from .guard/.fileignore')
+@click.option('-inc', '--include', nargs=1, help='include a word to scan by adding it to .guard/.fileignore')
+@click.option('--add', '-a', nargs=1, help='Add new words to .guard/.keyignore')
+@click.option('-i', '--init', is_flag=True, help='Initialize the key_guard')
 @click.option('-l', '--list', is_flag=True, help='List all the guarded words')
 @click.option('-s', '--scan', is_flag=True, help='Scan the project for any key or token')
 @click.argument('path', type=click.Path(dir_okay=True), default=cwd, required=False)
-@click.argument('file', type=str, required=False)
-def cli(path: str, add: bool, remove: bool, exclude: bool, include: bool, list: bool, scan: bool, file: str) -> None:
+# @click.argument('add', type=str, required=False)
+def cli(path: str,  list: str, scan: bool, add: str, init: bool, exempt: str, include: str) -> None:
     """CLI tool to scan a project for any key or token
     """
+    if init:
+        # create .guard folder and create .fileignore and .keyignore files
+        try:
+            os.mkdir('.guard')
+            os.chdir('.guard')
+            click.secho("Initializing the key_guard", fg='green')
+            click.secho("Creating .guardignore files", fg='green')
+            with open('.fileignore', 'w') as f:
+                f.writelines(['.git\n', '.guard\n', '.vscode\n', '.idea\n', '.pytest_cache\n',
+                              '__pycache__\n', 'venv\n', 'node_modules\n', '.env\n', '.venv\n'])
+                f.close()
+
+            with open('.keyignore', 'w') as f:
+                f.writelines(['key =\n', 'access_key =\n', 'auth_key =\n',
+                              'password =\n', 'secret =\n', 'token =\n', 'access_token =\n'])
+                f.close()
+        except FileExistsError:
+            click.secho("key_guard is already initialized", fg='yellow')
+            pass
+
     if scan:
         def searchText(path):
+            guarded_words = [str(word.strip())
+                             for word in open('.guard/.keyignore').readlines()]
+            exempted_files = [str(file.strip())
+                              for file in open('.guard/.fileignore').readlines()]
 
+            os.chdir('..')
             os.chdir(path)
             path = os.listdir(path)
             for fname in path[::-1]:
@@ -48,37 +65,71 @@ def cli(path: str, add: bool, remove: bool, exclude: bool, include: bool, list: 
                     print(f"Scanning {abs_path}")
                     searchText(abs_path)
                     path.remove(os.path.basename(abs_path))
-                    # move back to parent directory
                     os.chdir('..')
         searchText(path)
         click.echo(click.style("\nScanning completed successfully", fg='green'))
 
+    if list:
+        guarded_words = [str(word.strip())
+                         for word in open('.guard/.keyignore').readlines()]
+        click.echo(guarded_words)
+
+    # add new words to .guard/.keyignore
     if add:
-        guarded_words.append(add)
-        click.echo(click.style(
-            f"Added '{add}' to the list of guarded words", fg='green'))
-
-    if remove:
+        guarded_words = [str(word.strip())
+                         for word in open('.guard/.keyignore').readlines()]
         try:
-            guarded_words.remove(remove)
-            click.echo(f"Removed {remove} from the list of guarded words")
-        except ValueError:
-            click.echo(f"{remove} not found in the list of guarded words")
+            with open('.guard/.keyignore', 'a') as f:
+                if add not in guarded_words:
+                    f.write(f"{add}\n")
+                    f.close()
+                    click.secho(
+                        f"Added `{add}` to .keyignore", fg='green')
+                else:
+                    click.secho(
+                        f"`{add}` already exists in .guard/.keyignore", fg='yellow')
+        except FileNotFoundError:
+            click.secho("Please initialize the key_guard first", fg='red')
 
-    if exclude:
-        exempted_files.append(exclude)
-        click.echo(f"Excluded {exclude} from the scan")
+    if exempt:
+        exempted_files = [str(file.strip())
+                          for file in open('.guard/.fileignore').readlines()]
+        try:
+            with open('.guard/.fileignore', 'a') as f:
+                if exempt not in exempted_files:
+                    f.write(f"{exempt}\n")
+                    f.close()
+                    click.secho(
+                        f"Added `{exempt}` to .fileignore", fg='green')
+                else:
+                    click.secho(
+                        f"`{exempt}` already exists in .guard/.fileignore", fg='yellow')
+        except FileNotFoundError:
+            click.secho("Please initialize the key_guard first", fg='red')
 
     if include:
+        exempted_files = [str(file.strip())
+                          for file in open('.guard/.fileignore').readlines()]
         try:
-            exempted_files.remove(include)
-            click.echo(f"Included {include} in the scan")
-        except ValueError:
-            click.echo(f"{include} not found in the list of excluded files")
-    if list:
-        click.echo(guarded_words)
-    else:
-        click.echo("Invalid command. Kindly check the help for more details")
+            with open('.guard/.fileignore', 'r') as f:
+                lines = f.readlines()
+                f.close()
+            with open('.guard/.fileignore', 'w') as f:
+                for line in lines:
+                    if line.strip("\n") != include:
+                        f.write(line)
+                    else:
+                        lines.remove(line)
+                        exempted_files.remove(line.strip("\n"))
+                        click.secho(
+                            f"Removed `{include}` from .fileignore", fg='green')
+
+                f.close()
+        except FileNotFoundError:
+            click.secho("Please initialize the key_guard first", fg='red')
+    elif not init and not scan and not list and not add and not exempt and not include:
+        click.secho(
+            "Welcome to key_guard! Use the `--help` option for the list of options and commands available for this tool.", fg='green')
 
 
 if __name__ == '__main__':
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read()
+setup(
+    name='key_guard',
+    version='0.0.1',
+    author='Jerry Buaba',
+    author_email='buabajerry@gmail.com',
+    license='MIT',
+    description='A CLI tool to help you guard your sensitive information from being pushed to a remote repository',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/buabaj/key-guard',
+    py_modules=['key_guard'],
+    packages=find_packages(),
+    install_requires=[requirements],
+    python_requires='>=3.8',
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ],
+    entry_points='''
+        [console_scripts]
+        key-guard=key_guard:cli
+    '''
+)
